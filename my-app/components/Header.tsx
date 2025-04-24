8ad035c63fec875cdb82db6bc6bd79958f333d30
Walther Oliveira <waltherpires@hotmail.com>
2025-04-24 03:43:47 -0300

    fix: navegacao na navbar

--- my-app/components/Header.tsx
+++ my-app/components/Header.tsx
@@ -14,98 +14,114 @@ export default function Navbar() {
           iMoodly
         </Link>
         <div className="flex">
-          {isLoggedIn && 
-          <>
-                <nav className="hidden md:flex gap-4">
-            <Link href="/dashboard">
-              <Button variant="ghost">Dashboard</Button>
-            </Link>
-            <Link href="/diary">
-              <Button variant="ghost">Diário</Button>
-            </Link>
-            <Link href="/messages">
-              <Button variant="ghost">
-                Mensagens
-              </Button>
-            </Link>
-          </nav>
+          {isLoggedIn && (
+            <>
+              <nav className="hidden md:flex gap-4">
+                <Link href="/dashboard">
+                  <Button variant="ghost">Dashboard</Button>
+                </Link>
+                <Link href="/diary">
+                  <Button variant="ghost">Diário</Button>
+                </Link>
+                <Link href="/messages">
+                  <Button variant="ghost">Mensagens</Button>
+                </Link>
+              </nav>
 
-          <div className="md:hidden">
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button variant="ghost" size="icon">
-                  <Menu className="h-6 w-6" />
-                </Button>
-              </SheetTrigger>
-              <SheetContent side="right">
-                <nav className="flex flex-col gap-4 mt-6">
-                  <Link href="/about">
-                    <Button variant="ghost" className="w-full justify-start">
-                      Sobre
+              <div className="md:hidden">
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                      <Menu className="h-6 w-6" />
                     </Button>
-                  </Link>
-                  <Link href="/services">
-                    <Button variant="ghost" className="w-full justify-start">
-                      Serviços
-                    </Button>
-                  </Link>
-                  <Link href="/login">
-                    <Button variant="ghost" className="w-full justify-start">
-                      Entrar
-                    </Button>
-                  </Link>
-                </nav>
-              </SheetContent>
-            </Sheet>
-          </div>
-          </>
-          }
+                  </SheetTrigger>
+                  <SheetContent side="right">
+                    <nav className="flex flex-col gap-4 mt-6">
+                      <Link href="/dashboard">
+                        <Button
+                          variant="ghost"
+                          className="w-full justify-start"
+                        >
+                          Dashboard
+                        </Button>
+                      </Link>
+                      <Link href="/diary">
+                        <Button
+                          variant="ghost"
+                          className="w-full justify-start"
+                        >
+                          Diário
+                        </Button>
+                      </Link>
+                      <Link href="/messages">
+                        <Button
+                          variant="ghost"
+                          className="w-full justify-start"
+                        >
+                          Mensagens
+                        </Button>
+                      </Link>
+                    </nav>
+                  </SheetContent>
+                </Sheet>
+              </div>
+            </>
+          )}
           {!isLoggedIn && (
             <>
-                      <nav className="hidden md:flex gap-4">
-            <Link href="/about">
-              <Button variant="ghost">Sobre</Button>
-            </Link>
-            <Link href="/services">
-              <Button variant="ghost">Serviços</Button>
-            </Link>
-            <Link href="/login">
-              <Button variant="ghost" className="w-full justify-start">
-                Entrar
-              </Button>
-            </Link>
-          </nav>
+              <nav className="hidden md:flex gap-4">
+                <Link href="/about">
+                  <Button variant="ghost">Sobre</Button>
+                </Link>
+                <Link href="/services">
+                  <Button variant="ghost">Serviços</Button>
+                </Link>
+                <Link href="/login">
+                  <Button variant="ghost" className="w-full justify-start">
+                    Entrar
+                  </Button>
+                </Link>
+              </nav>
 
-          <div className="md:hidden">
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button variant="ghost" size="icon">
-                  <Menu className="h-6 w-6" />
-                </Button>
-              </SheetTrigger>
-              <SheetContent side="right">
-                <nav className="flex flex-col gap-4 mt-6">
-                  <Link href="/about">
-                    <Button variant="ghost" className="w-full justify-start">
-                      Sobre
+              <div className="md:hidden">
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                      <Menu className="h-6 w-6" />
                     </Button>
-                  </Link>
-                  <Link href="/services">
-                    <Button variant="ghost" className="w-full justify-start">
-                      Serviços
-                    </Button>
-                  </Link>
-                  <Link href="/login">
-                    <Button variant="ghost" className="w-full justify-start">
-                      Entrar
-                    </Button>
-                  </Link>
-                </nav>
-              </SheetContent>
-            </Sheet>
-          </div>
+                  </SheetTrigger>
+                  <SheetContent side="right">
+                    <nav className="flex flex-col gap-4 mt-6">
+                      <Link href="/about">
+                        <Button
+                          variant="ghost"
+                          className="w-full justify-start"
+                        >
+                          Sobre
+                        </Button>
+                      </Link>
+                      <Link href="/services">
+                        <Button
+                          variant="ghost"
+                          className="w-full justify-start"
+                        >
+                          Serviços
+                        </Button>
+                      </Link>
+                      <Link href="/login">
+                        <Button
+                          variant="ghost"
+                          className="w-full justify-start"
+                        >
+                          Entrar
+                        </Button>
+                      </Link>
+                    </nav>
+                  </SheetContent>
+                </Sheet>
+              </div>
             </>
-          ) }
+          )}
 
           <ThemeToggle />
         </div>
