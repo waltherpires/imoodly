90b6062772a5447821b5e8f1023bf4d242295a76
Walther Oliveira <waltherpires@hotmail.com>
2025-05-01 04:38:06 -0300

    feature: adiciona botao de logout

--- my-app/components/my-ui/LogoutButton.tsx
+++ my-app/components/my-ui/LogoutButton.tsx
@@ -3,16 +3,19 @@
 import { useRouter } from 'next/navigation'
 import axios from 'axios'
 import { Button } from '../ui/button'
+import { useAuth } from '@/contexts/useAuth';
 
 interface LogoutButton {
     className?: string;
 }
 
 export default function LogoutButton({ className }: LogoutButton) {
+  const { setUserId } = useAuth();
   const router = useRouter()
 
   const handleLogout = async () => {
     await axios.post('/api/logout')
+    setUserId(null);
     router.push('/login')
   }
 
