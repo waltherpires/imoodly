4ad7c835104d65d00089d02b8c03c1a77b58409b
Walther Oliveira <waltherpires@hotmail.com>
2025-05-22 23:32:31 -0300

    fix: tamanho parent de ChartContainer

--- my-app/components/sections/dashboard/DashboardChart/Chart.tsx
+++ my-app/components/sections/dashboard/DashboardChart/Chart.tsx
@@ -184,7 +184,7 @@ export default function Chart({
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.95 }}
             transition={{ duration: 0.3 }}
-            className="h-[400px]"
+            className="h-[500px]"
           >
             <ChartContainer
               config={chartConfig}
